1f5ec4d12083f43ec98d2311196718d16d1f5c54
Madhan-Kumar-Gali <gmadhang9@gmail.com>
2019-11-04 21:27:21 +0530

    UserRegistration 1

--- src/main/java/technicalblog/model/User.java
+++ src/main/java/technicalblog/model/User.java
@@ -1,25 +1,45 @@
 package technicalblog.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username")
     private String username;
+    @Column(name = "password")
     private String password;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "profile_id")
+    private UserProfile profile;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
     }
 
-    @SuppressWarnings("unused")
     public void setUsername(String username) {
         this.username = username;
     }
 
-    @SuppressWarnings("unused")
     public String getPassword() {
         return password;
     }
 
-    @SuppressWarnings("unused")
     public void setPassword(String password) {
         this.password = password;
     }
